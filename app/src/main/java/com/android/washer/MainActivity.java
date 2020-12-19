137579c88aff216da4326a742efc3f67d9a0d838
Konstantinos Kolioulis <konstantinoskol@hotmail.com>
2020-12-19 16:43:38 +0200

    Add listeners in MainActivity

--- app/src/main/java/com/android/washer/MainActivity.java
+++ app/src/main/java/com/android/washer/MainActivity.java
@@ -2,12 +2,36 @@ package com.android.washer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private Button scanButton, addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scanButton = findViewById(R.id.scanBtn);
+        addButton = findViewById(R.id.addBtn);
+        addListeners();
+    }
+
+    private void addListeners() {
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
