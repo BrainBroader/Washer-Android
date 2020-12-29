99e0dd4e6004e8f05f5d8bd25ca8fa6692084ab5
kkoliou <konstantinoskol@hotmail.com>
2020-12-29 14:01:02 +0200

    Update buttons state in AddFriendlyNameActivity and add Toasts

--- app/src/main/java/com/android/washer/AddFriendlyNameActivity.java
+++ app/src/main/java/com/android/washer/AddFriendlyNameActivity.java
@@ -2,9 +2,12 @@ package com.android.washer;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +21,7 @@ public class AddFriendlyNameActivity extends AppCompatActivity {
     private EditText friendlyNameEditText;
     private Button deleteButton, saveButton;
     private WasherModel washer;
+    private String friendlyName;
 
     private final String SHARED_PREFS = "sharedPrefs";
 
@@ -35,13 +39,28 @@ public class AddFriendlyNameActivity extends AppCompatActivity {
         deleteButton = findViewById(R.id.deleteButton);
         saveButton = findViewById(R.id.saveButton);
         handleButtonListeners();
+        handleTextWatcher();
         setupTextField();
+        setupButtons();
     }
 
     private void setupTextField() {
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-        String friendlyName = sharedPreferences.getString(washer.getId(), "");
+        friendlyName = sharedPreferences.getString(washer.getId(), "");
         friendlyNameEditText.setText(friendlyName);
+    }
+
+    private void setupButtons() {
+        saveButton.setEnabled(false);
+        saveButton.setAlpha((float) 0.5);
+
+        if (friendlyName.equals("")) {
+            deleteButton.setEnabled(false);
+            deleteButton.setAlpha((float) 0.5);
+        } else {
+            deleteButton.setEnabled(true);
+            deleteButton.setAlpha((float) 1.0);
+        }
     }
 
     private void handleButtonListeners() {
@@ -53,6 +72,10 @@ public class AddFriendlyNameActivity extends AppCompatActivity {
                 editor.putString(washer.getId(), "");
                 editor.apply();
                 updateRecyclerView();
+                friendlyName = "";
+                setupButtons();
+                friendlyNameEditText.setText("");
+                Toast.makeText(getApplicationContext(), "Η φιλική ονομασία διαγράφτηκε.", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -61,11 +84,41 @@ public class AddFriendlyNameActivity extends AppCompatActivity {
             public void onClick(View v) {
                 SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString(washer.getId(), friendlyNameEditText.getText().toString());
+                String newFriendlyName = friendlyNameEditText.getText().toString();
+                editor.putString(washer.getId(), newFriendlyName);
                 editor.apply();
                 updateRecyclerView();
+                friendlyName = newFriendlyName;
+                setupButtons();
+                Toast.makeText(getApplicationContext(), "Η φιλική ονομασία αποθηκεύτηκε!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void handleTextWatcher() {
+        friendlyNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                changeButtonState();
+            }
+        });
+    }
+
+    private void changeButtonState() {
+        String text = friendlyNameEditText.getText().toString();
+        if (text.equals("")) {
+            saveButton.setEnabled(false);
+            saveButton.setAlpha((float) 0.5);
+        } else {
+            saveButton.setEnabled(true);
+            saveButton.setAlpha((float) 1.0);
+        }
     }
 
     private void updateRecyclerView() {
