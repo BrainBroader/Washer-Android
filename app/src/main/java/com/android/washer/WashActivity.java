95afbd25f4b5f75a259a5c9d759b24d27ba2dd2e
kkoliou <konstantinoskol@hotmail.com>
2021-01-09 22:43:11 +0200

    Handle wash cancelation

--- app/src/main/java/com/android/washer/WashActivity.java
+++ app/src/main/java/com/android/washer/WashActivity.java
@@ -1,8 +1,10 @@
 package com.android.washer;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -10,13 +12,16 @@ import android.widget.ImageView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 import me.itangqi.waveloadingview.WaveLoadingView;
 
 public class WashActivity extends AppCompatActivity {
 
     WaveLoadingView waveLoadingView;
     Button cancelButton;
-    ImageView doneImageView;
+    ImageView statusImageView;
+    CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,8 +30,9 @@ public class WashActivity extends AppCompatActivity {
 
         waveLoadingView = findViewById(R.id.waveLoadingView);
         cancelButton = findViewById(R.id.cancelWashButton);
-        doneImageView = findViewById(R.id.doneIcon);
+        statusImageView = findViewById(R.id.statusIcon);
         setupProgressBar();
+        handleCancel();
     }
 
     private void setupProgressBar() {
@@ -45,19 +51,22 @@ public class WashActivity extends AppCompatActivity {
         final int maxTime = 100000;
 
         //increase progress every 500ms
-        new CountDownTimer(maxTime, 100) {
+        countDownTimer = new CountDownTimer(maxTime, 100) {
             public void onTick(long millisUntilFinished) {
                 int currentProg = ++progress[0];
                 waveLoadingView.setProgressValue(currentProg);
                 if (currentProg >= 100) {
                     this.cancel();
                     didWashFinish();
+                    statusImageView.setBackgroundResource(R.drawable.done_icon);
                 }
             }
+
             public void onFinish() {
                 waveLoadingView.setProgressValue(100);
                 this.cancel();
                 didWashFinish();
+                statusImageView.setBackgroundResource(R.drawable.done_icon);
             }
         }.start();
     }
@@ -65,6 +74,30 @@ public class WashActivity extends AppCompatActivity {
     private void didWashFinish() {
         waveLoadingView.setVisibility(View.GONE);
         cancelButton.setVisibility(View.GONE);
-        doneImageView.setVisibility(View.VISIBLE);
+        statusImageView.setVisibility(View.VISIBLE);
+    }
+
+    private void handleCancel() {
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialAlertDialogBuilder(WashActivity.this)
+                        .setTitle("Ακύρωση")
+                        .setMessage("Είσαι σίγουρος ότι θες να σταματήσεις την πλύση;")
+                        .setPositiveButton("ΝΑΙ", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                countDownTimer.cancel();
+                                didWashFinish();
+                                statusImageView.setBackgroundResource(R.drawable.cancel_icon);
+                            }
+                        })
+                        .setNegativeButton("ΟΧΙ", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {}
+                        })
+                        .show();
+            }
+        });
     }
 }
